03e2f87353cd2154c78128a813bca0dcf03d4d33
Robert Usey <ruusey@gmail.com>
2019-04-03 14:19:22 -0400

    AutoReef for Chrome on Windows. Other platforms soon come.

--- src/main/java/com/reef/Chrome.java
+++ src/main/java/com/reef/Chrome.java
@@ -1,8 +1,6 @@
 package com.reef;
 
 import java.awt.AWTException;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,7 +8,6 @@ import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Chrome {
@@ -18,6 +15,7 @@ public class Chrome {
 	static VirtualKeyBoard kb = null;
 	static String email = "";
 	static String password = "";
+	static String preOut = "[AutoReef] ";
 	public Chrome() {
 		// TODO Auto-generated constructor stub
 	}
@@ -28,25 +26,20 @@ public class Chrome {
 		try {
 			kb=new VirtualKeyBoard();
 		} catch (AWTException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
 		Properties p = new Properties();
-		
 		try {
 			InputStream is = classloader.getResourceAsStream("settings.properties");
 			p.load(is);
 			is.close();
 			email = p.getProperty("email");
 			password = p.getProperty("password");
-			System.out.println("Loaded account from file ("+email+")");
-			
+			System.out.println(preOut+"Loaded account from file ("+email+")");
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -54,21 +47,21 @@ public class Chrome {
 		while(driver.getTitle()==null) {
 			delayAction(1000);
 		}
-		System.out.println("Succesfully connected to: "+driver.getCurrentUrl());
+		System.out.println(preOut+"Succesfully connected to: "+driver.getCurrentUrl());
 		delayAction(1000);
 		clickEmail();
 		clickPassword();
-
 		clickSignIn();
 		delayAction(1000);
 		clickFirstCourse();
+		boolean displayJoinAttmpts = true;
 		while(!joinAvailable()) {
-			System.out.println("No Sessions Available, idling...");
-			delayAction(5000);
-			
+			if(displayJoinAttmpts) {
+				System.out.println(preOut+"No Sessions Available, Idling...");
+				displayJoinAttmpts=false;
+			}
+			delayAction(5000);	
 		}
-		
-		
 	}
 	public static void clickEmail() {
 		driver.findElement(By.id("userEmail")).click();
@@ -79,16 +72,12 @@ public class Chrome {
 		driver.findElement(By.id("userPassword")).click();
 		driver.findElement(By.id("userPassword")).sendKeys(password);
 		delayAction(1000);
-		
 	}
 	public static void clickSignIn() {
 		driver.findElement(By.id("sign-in-button")).click();
 		delayAction(1000);
-		
 	}
 	public static void clickFirstCourse() {
-		By by = By.className("ng-scope");
-		
 		driver.findElement(By.tagName("a")).click();
 		delayAction(1000);
 		
@@ -109,5 +98,4 @@ public class Chrome {
 			e.printStackTrace();
 		}
 	}
-
 }
